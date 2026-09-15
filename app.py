a79661d512d50b02254fca7023ce98ea71d89125
Infin8tyX <devp18@NYCSTUDENTS.NET>
2026-09-15 09:20:04 -0400

    Turtle Square

--- app.py
+++ app.py
@@ -3,5 +3,17 @@ from turtle import *
 t = Turtle()
 t.shape('turtle')
 
-t.forward(200)
+""" t.forward(200) """
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+
 
