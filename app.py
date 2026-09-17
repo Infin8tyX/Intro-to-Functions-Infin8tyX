f37f5d275ca9a2b66489ecde74b01c7087ce8e5b
Infin8tyX <devp18@NYCSTUDENTS.NET>
2026-09-17 09:20:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-for i in range(3):
-    print(i)
 
-def square():
-    for i in range(4):
-     t.forward(100)
-     t.left(90)
-for i in range(5):
-    square()
-    t.left(5)
-
+def square(x):
+     y= 10
+     for i in range(5):
+        t.forward(100)
+        t.left(50)
+        print(i)
+        y-=1
+        square(100)
+square(100)
 
